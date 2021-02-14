7d9b67c9b2df12edc62a707e8ea2e184e646cf3f
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-02-14 07:33:54 +0300

    msg

--- src/main/java/org/dummy/App.java
+++ src/main/java/org/dummy/App.java
@@ -1,6 +1,10 @@
 package org.dummy;
 
-import io.undertow.Undertow;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.HandlerList;
 
 /**
  * Main class.
@@ -10,11 +14,26 @@ public class App {
     private static final String HOST = "0.0.0.0";
     private static final int PORT = 8080;
 
-    public static void main(String[] args) {
-        Undertow server = Undertow.builder()
-                .addHttpListener(PORT, HOST)
-                .setHandler(new Router()).build();
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
+
+        // Setup HTTP Connector
+        HttpConfiguration httpConf = new HttpConfiguration();
+
+        // Establish the HTTP ServerConnector
+        ServerConnector httpConnector = new ServerConnector(server,
+                new HttpConnectionFactory(httpConf));
+        httpConnector.setPort(PORT);
+        httpConnector.setHost(HOST);
+        server.addConnector(httpConnector);
+
+        // Add a Handlers for requests
+        HandlerList handlers = new HandlerList();
+        handlers.addHandler(new CommonHandler());
+        server.setHandler(handlers);
+
         server.start();
+        server.join();
     }
 
 }
---
+++ src/main/java/org/dummy/CommonHandler.java
@@ -0,0 +1,151 @@
+package org.dummy;
+
+import java.io.*;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import static org.dummy.EmptinessUtils.isNotEmpty;
+import static org.dummy.HtmlToPdfUtils.*;
+import static org.dummy.HtmlToPdfUtils.PrinterOptions.TMP_DIR;
+import static org.dummy.OsUtils.*;
+
+/**
+ * Common {@link AbstractHandler}.
+ */
+public class CommonHandler extends AbstractHandler {
+
+    private static final String JETTY_TMP = "jetty-" + getRandomUUID();
+    private static final String HTML = "html";
+    private static final String CHROMIUM = "chromium";
+    private static final String POST = "POST";
+    private static final String TEXT_PLAIN = "text/plain";
+    private static final String STATUS_UP = "{\"status\":\"UP\"}";
+    private static final String APPLICATION_PDF = "application/pdf";
+    private static final String PDF_ATTACHED = "attachment;filename=\"" + RESULT_PDF + "\"";
+    private static final String INDEX_HTML_NOT_FOUND = INDEX_HTML + " not found";
+    private static final String NO_MULTIPART = "No multipart";
+    private static final int INTERNAL_SERVER_ERROR = 500;
+
+    private MultipartConfigElement multipartConfig;
+
+    /**
+     * Constructor.
+     */
+    public CommonHandler() {
+        super();
+        deleteFilesAndDirectories(TMP_DIR);
+        Path multipartTmpDir = TMP_DIR.resolve(JETTY_TMP);
+        createDirectory(multipartTmpDir);
+        this.multipartConfig = new MultipartConfigElement(multipartTmpDir.toString());
+    }
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        if ((request.getRequestURI().contains(HTML) || request.getRequestURI().contains(CHROMIUM))
+                && request.getMethod().equalsIgnoreCase(POST)) {
+            request.setAttribute(Request.MULTIPART_CONFIG_ELEMENT, multipartConfig);
+            processParts(request, response);
+        } else {
+            health(response);
+        }
+        baseRequest.setHandled(true);
+    }
+
+    private static void storeParts(HttpServletRequest request, HtmlToPdfUtils.PrinterOptions po) throws IOException, ServletException {
+        for (Part part : request.getParts()) {
+            String filename = part.getSubmittedFileName();
+            if (isNotEmpty(filename)) {
+                // ensure we don't have "/" and ".." in the raw form.
+                filename = URLEncoder.encode(filename, DEFAULT_CHARSET_NAME);
+                Path file = po.getWorkdir().resolve(filename);
+                InputStream inputStream = null;
+                OutputStream outputStream = null;
+                try {
+                    inputStream = part.getInputStream();
+                    outputStream = Files.newOutputStream(file, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+                    inputStream.transferTo(outputStream);
+                } catch (Exception e) {
+                    //
+                } finally {
+                    if (null != inputStream) {
+                        inputStream.close();
+                    }
+                    if (null != outputStream) {
+                        outputStream.close();
+                    }
+                }
+            }
+        }
+    }
+
+    private static void convert(HttpServletResponse response, HtmlToPdfUtils.PrinterOptions po) throws IOException {
+        htmlToPdf(po);
+        Path resultPdf = po.getWorkdir().resolve(RESULT_PDF);
+        if (resultPdf.toFile().exists() && resultPdf.toFile().isFile()) {
+            response.setContentType(APPLICATION_PDF);
+            response.setHeader("Content-Disposition", PDF_ATTACHED);
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+            try {
+                inputStream = Files.newInputStream(resultPdf);
+                outputStream = response.getOutputStream();
+                inputStream.transferTo(outputStream);
+            } catch (Exception e) {
+                //
+            } finally {
+                if (null != inputStream) {
+                    inputStream.close();
+                }
+                if (null != outputStream) {
+                    outputStream.close();
+                }
+            }
+        } else {
+            internalServerError(response, po.getWrapper().getOutputString() + DELIMITER_NEW_LINE + po.getWrapper().getErrorString());
+        }
+    }
+
+    private static void processParts(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (!request.getParts().isEmpty()) {
+            HtmlToPdfUtils.PrinterOptions po = new HtmlToPdfUtils.PrinterOptions();
+            String url = request.getRequestURI();
+            po.printoutSettings(url);
+            po.buildOsCommandWrapper();
+            createDirectory(po.getWorkdir());
+            storeParts(request, po);
+            Path indexHtml = po.getWorkdir().resolve(INDEX_HTML);
+            if (indexHtml.toFile().exists() && indexHtml.toFile().canRead()) {
+                convert(response, po);
+            } else {
+                internalServerError(response, INDEX_HTML_NOT_FOUND);
+            }
+            deleteFilesAndDirectories(po.getWorkdir());
+        } else {
+            internalServerError(response, NO_MULTIPART);
+        }
+    }
+
+    private static void plainTextUtf8Response(HttpServletResponse response) {
+        response.setContentType(TEXT_PLAIN);
+        response.setCharacterEncoding(DEFAULT_CHARSET_NAME);
+    }
+
+    private static void health(HttpServletResponse response) throws IOException {
+        plainTextUtf8Response(response);
+        response.getWriter().print(STATUS_UP);
+    }
+
+    private static void internalServerError(HttpServletResponse response, String reason) throws IOException {
+        plainTextUtf8Response(response);
+        response.setStatus(INTERNAL_SERVER_ERROR);
+        response.getWriter().print(reason);
+    }
+}
