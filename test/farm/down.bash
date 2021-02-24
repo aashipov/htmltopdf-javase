7fbb7d7e083a2c7e912d7a600850d7f6b5e63c56
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-02-24 08:20:45 +0300

    simpler array elements concat

--- test/farm/down.bash
+++ test/farm/down.bash
@@ -3,11 +3,11 @@
 set -x
 
 NODE_NAMES=("htmltopdf-javase1" "htmltopdf-javase2" "htmltopdf-javase3")
-
-# https://stackoverflow.com/a/49167382
 NODE_NAMES_SPACE_SEPARATED=""
-printf -v NODE_NAMES_SPACE_SEPARATED ' %s' "${NODE_NAMES[@]}"
-NODE_NAMES_SPACE_SEPARATED=${NODE_NAMES_SPACE_SEPARATED:1}
+for node_name in "${NODE_NAMES[@]}"
+do
+    NODE_NAMES_SPACE_SEPARATED+=" ${node_name}"
+done
 
 HAPROXY=htmltopdf-javase-haproxy
 NETWORK_NAME=htmltopdf-javase
