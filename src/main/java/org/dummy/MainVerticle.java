12d1f964317432843660a3e1efa2390652095eea
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-03-08 08:04:41 +0300

    msg

--- src/main/java/org/dummy/MainVerticle.java
+++ src/main/java/org/dummy/MainVerticle.java
@@ -35,7 +35,6 @@ public class MainVerticle extends AbstractVerticle {
                 .setTcpQuickAck(true)
                 .setPort(PORT);
         HttpServer server = super.vertx.createHttpServer(httpServerOptions);
-
         server.requestHandler(router);
         server.listen();
     }
