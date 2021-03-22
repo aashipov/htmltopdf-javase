0ea28b9060504989618b3259d9bda4f08bc07035
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-03-22 15:15:53 +0300

    msg

--- src/main/kotlin/org/dummy/ChromiumWrapper.kt
+++ src/main/kotlin/org/dummy/ChromiumWrapper.kt
@@ -1,33 +1,54 @@
 package org.dummy
 
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.take
 import org.hildan.chrome.devtools.domains.page.PrintToPDFRequest
 import org.hildan.chrome.devtools.protocol.ChromeDPClient
 import org.hildan.chrome.devtools.targets.*
 
 /**
- * Wrapper around [ChromeBrowserSession].
+ * [ChromeBrowserSession] wrapper.
  */
 class ChromiumWrapper {
 
     companion object {
-        private suspend fun browserSession() : ChromeBrowserSession {
+        private suspend fun browserSession(): ChromeBrowserSession {
             return ChromeDPClient("http://0.0.0.0:9222").webSocket()
         }
+
         private val browserSession: ChromeBrowserSession =
             runBlocking {
                 browserSession()
             }
+
         private suspend fun pdfInner(url: String, printToPDFRequest: PrintToPDFRequest): String {
-            val pageSession: ChromePageSession = browserSession.attachToNewPageAndAwaitPageLoad(url)
-            Thread.sleep(100)
+            val pageSession: ChromePageSession = browserSession.attachToNewPage(url)
+            pageSession.page.enable()
+            //https://stackoverflow.com/a/59109105
+            try {
+                coroutineScope {
+                    launch {
+                        // three events as page loads
+                        pageSession.page.events().take(3).collect()
+                        //and a bit more for rendering to finish
+                        Thread.sleep(100)
+                        this@coroutineScope.cancel()
+                    }
+                }
+
+            } catch (e: CancellationException) {
+                //ignore
+            }
             val pdf: String = pageSession.page.printToPDF(printToPDFRequest).data
             pageSession.close()
             return pdf
         }
+
         @JvmStatic
-        fun pdf(url: String, printToPDFRequest: PrintToPDFRequest): String = runBlocking {
-            pdfInner(url, printToPDFRequest)
-        }
+        fun pdf(url: String, printToPDFRequest: PrintToPDFRequest): String =
+            runBlocking {
+                pdfInner(url, printToPDFRequest)
+            }
     }
 }
